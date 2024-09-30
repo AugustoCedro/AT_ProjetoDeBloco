a2eaf94d36946cb01aeeb08aeb26cb5914aed91f
AugustoRajao <augustocedro9@gmail.com>
2024-09-30 18:02:18 -0300

    Adicionando componente SearcBar

---
+++ src/Components/SearchBar.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+function SearchBar() {
+    return (
+        <>
+        <input
+        type="text"
+        placeholder="Digite o nome do filme"
+      />
+      </>
+    );
+  }
+  
+  export default SearchBar;
+  
